6191aeaacb05f77d43c3c6c7153703353a212b3f
powdergotpower <anshjotsinghsidhu@gmail.com>
2025-08-29 14:05:08 +0530

    Update ping.py

--- modules/ping.py
+++ modules/ping.py
@@ -1,17 +1,12 @@
 from telethon import events
-from telethon.tl.functions.messages import UpdatePinnedMessageRequest
+import time
 
 def register(client):
 
-    # Only trigger on exact .pin command
-    @client.on(events.NewMessage(pattern=r'^\.pin$'))
-    async def pin_handler(event):
-        if not event.is_group:
-            await event.reply("This command is for groups only.")
-            return
-        if not event.is_reply:
-            await event.reply("Reply to a message to pin it.")
-            return
-        msg = await event.get_reply_message()
-        await client(UpdatePinnedMessageRequest(peer=event.chat_id, id=msg.id, silent=True))
-        await event.reply("Message pinned ✅")
+    @client.on(events.NewMessage(pattern=r'^\.ping$'))
+    async def ping_handler(event):
+        start = time.time()
+        msg = await event.reply("Pong! 🏓")
+        end = time.time()
+        response_time = int((end - start) * 1000)
+        await msg.edit(f"Pong! 🏓\nResponse time: {response_time}ms")
