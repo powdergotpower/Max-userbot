d40110807f6b7dee9ae64a6f154a7391750c7f7c
powdergotpower <anshjotsinghsidhu@gmail.com>
2025-08-29 16:27:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-# Config.py for Max-userbot
+# Config.py
+API_ID = 22071176
+API_HASH = '7ed5401b625a0a4d3c45caf12c87f166'
 
-# Temporary folder for cloning profile photos
-TEMP_DIR = "./temp"
+# Optional settings
+BOTLOG = False        # Disable logging messages
+BOTLOG_CHATID = None  # No chat ID for logs
+
+TEMP_DIR = "/data/data/com.termux/files/home/Max-userbot/temp/"
